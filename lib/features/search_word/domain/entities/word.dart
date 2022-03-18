724553abc4866641c08c89a8c1e8f64b63c436cf
wugq <wugq.dev@gmail.com>
2022-03-18 15:43:56 +0800

    refactoring code

--- lib/features/search_word/domain/entities/word.dart
+++ lib/features/search_word/domain/entities/word.dart
@@ -19,7 +19,10 @@ class Pronunciation extends Equatable {
   final String audio;
   final String text;
 
-  const Pronunciation(this.audio, this.text);
+  const Pronunciation({
+    required this.audio,
+    required this.text,
+  });
 
   @override
   List<Object?> get props => [audio, text];
